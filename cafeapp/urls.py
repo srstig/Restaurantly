1318f62df491f6d4437295b224a43ab5da774552
vex-spec <ruoroabigael@gmail.com>
2025-03-14 16:28:50 +0300

    groupproject

--- cafeapp/urls.py
+++ cafeapp/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('starter/', views.starter, name='starter'),
     path('about/', views.about, name='about'),
+    path('booking/', views.booking, name='booking'),
+    path('chefs/', views.chefs, name='chefs'),
+    path('contact/', views.contact, name='contact'),
+    path('events/', views.events, name='events'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('menu/', views.menu, name='menu'),
+    path('specials/', views.specials, name='specials'),
+    path('testimonials/', views.testimonials, name='testimonials'),
+    path('whyus/', views.whyus, name='whyus'),
 ]
--- cafeapp/views.py
+++ cafeapp/views.py
@@ -12,3 +12,27 @@ def about(request):
 
 def menu(request):
     return render(request, 'menu.html')
+def booking(request):
+    return render(request, 'booking.html')
+
+def chefs(request):
+    return render(request, 'chefs.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def events(request):
+    return render(request, 'events.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
+def specials(request):
+    return render(request, 'specials.html')
+
+def testimonials(request):
+    return render(request, 'testimonials.html')
+
+def whyus(request):
+    return render(request, 'whyus.html')
